1c0f3db32816c49c722e9799eefa94988b08ac8b
WorldWebStudios <info@worldwebstudios.com>
2025-08-02 16:00:09 +0000

    Update register route to save new user fields

--- auth.py
+++ auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import LoginManager, login_user, logout_user, login_required
 from models import User, db
 from werkzeug.security import generate_password_hash, check_password_hash
+from forms import RegistrationForm
 
 auth = Blueprint('auth', __name__)
 login_manager = LoginManager()
@@ -12,31 +13,27 @@ def load_user(user_id):
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        if not username or not password:
-            flash("Username and password are required.")
-            return redirect(url_for('auth.register'))
-
-        existing_user = User.query.filter_by(username=username).first()
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(username=form.username.data).first()
         if existing_user:
-            flash("Username already taken.")
+            flash('Username already exists.')
             return redirect(url_for('auth.register'))
 
-        # First registered user becomes admin
-        is_admin = (User.query.count() == 0)
-
-        new_user = User(username=username, is_admin=is_admin)
-        new_user.set_password(password)
-        db.session.add(new_user)
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            name=form.name.data,
+            unit_number=form.unit_number.data,
+            phone_number=form.phone_number.data
+        )
+        user.set_password(form.password.data)
+        db.session.add(user)
         db.session.commit()
-
-        flash("Registration successful. Please log in.")
+        flash('Registration successful. You may now log in.')
         return redirect(url_for('auth.login'))
 
-    return render_template('register.html')
+    return render_template('register.html', form=form)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
